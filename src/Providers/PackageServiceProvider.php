f4f672207c5e64c152395b114845c1a15ef30a29
Jay Arabia <firebender.me@gmail.com>
2021-01-28 14:48:10 +0800

    Create package service provider

---
+++ src/Providers/PackageServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FireBender\Laravel\Complyant\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+
+{
+    /**
+     * 
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * 
+     */
+    public function register()
+    {
+    }
+
+}

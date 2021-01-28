47d11a599e29fc00fa4ed28a20aa6e95a3ae4954
Jay Arabia <firebender.me@gmail.com>
2021-01-28 15:49:31 +0800

    Require guzzle. Register our own client in service provider

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -5,6 +5,7 @@ namespace FireBender\Laravel\Complyant\Providers;
 use Illuminate\Support\ServiceProvider;
 use FireBender\Laravel\Complyant\Console\Commands\CreateApplicantCommand;
 use FireBender\Laravel\Complyant\Console\Commands\CreateApplicationCommand;
+use GuzzleHttp\Client as GuzzleClient;
 
 class PackageServiceProvider extends ServiceProvider
 
@@ -28,6 +29,16 @@ class PackageServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton('RestClient', function ($app) {
+
+            $config = [
+                'headers' => [
+                    'User-Agent' => 'Jay Arabia User Agent'
+                ]
+            ];
+
+            return new GuzzleClient($config);
+        });
     }
 
 }
